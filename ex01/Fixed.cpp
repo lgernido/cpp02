b9a7b4a41caa9a50e6acf23a421dd01b571b3410
Lucie Gernidos <lgernido@made-f0Br8s5.clusters.42paris.fr>
2024-05-14 12:48:29 +0200

    ex01 ok but fix display

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/12 12:48:38 by lgernido          #+#    #+#             */
+/*   Updated: 2024/05/14 12:37:52 by lgernido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/*CONSTRUCTORS AND DESTRUCTORS*/
+
+//Default Constructor
+
+Fixed::Fixed()
+{
+    std::cout << BOLD << "Default constructor" << RESET << " called" << \
+    std::endl;
+    number = 0;
+}
+
+Fixed::Fixed(const int number)
+{
+    std::cout << "Int constructor called" << std::endl;
+    this->number = number * static_cast<int>(pow(2, this->bits));
+}
+
+Fixed::Fixed(const float number)
+{
+    std::cout << "Float contructer called" << std::endl;
+    this->number = roundf(number * pow(2, this->bits));
+}
+
+//Default destructor
+
+Fixed::~Fixed()
+{
+    std::cout << BOLD << "Destructor" << RESET << " called" << \
+    std::endl;
+}
+
+//Copy constructor
+
+Fixed::Fixed(const Fixed& aFixed)
+{
+    std::cout << BOLD << "Copy" << RESET << " constructor called" << \
+    std::endl;
+    this->number = aFixed.getRawBits();
+}
+
+//Copy assignement operator
+
+Fixed& Fixed::operator=(const Fixed& aFixed)
+{
+    std::cout << BOLD << "Copy assignement operator" << RESET << " called" << \
+    std::endl;
+    this->number = aFixed.getRawBits(); 
+    return (*this);
+}
+
+/*PUBLIC METHODS*/
+
+int Fixed::getRawBits(void)const
+{
+    std::cout << BOLD << "getRawBits" << RESET << " member function called" << \
+    std::endl;
+    return(this->number);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << BOLD << "setRawBits" << RESET << " member function called" << \
+    std::endl; 
+    this->number = raw; 
+}
+
+float Fixed::toFloat(void)const
+{
+    return (this->number / pow(2, this->bits));
+}
+
+int Fixed::toInt(void)const
+{
+    return (this->number / static_cast<int>(pow(2, this->bits)));
+}
+
+/*OPERATOR OVERLOADING*/
+
+std::ostream& operator<<(std::ostream& os, const Fixed& aFixed)
+{
+    os << aFixed.toFloat();
+    return os;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/12 12:48:39 by lgernido          #+#    #+#             */
+/*   Updated: 2024/05/14 12:46:45 by lgernido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cmath>
+
+#define RED		"\033[0;31m"
+#define GREEN	"\033[0;32m"
+#define YELLOW	"\033[0;33m"
+#define ORANGE "\033[38;5;208m"
+#define BLUE	"\033[0;34m"
+#define MAGENTA	"\033[0;35m"
+#define CYAN	"\033[0;36m"
+#define WHITE	"\033[0;37m"
+#define BOLD       "\033[1m"
+#define UNDERLINE  "\033[4m"
+#define ITALIC     "\033[3m"
+#define RESET      "\033[0m"
+
+class Fixed
+{
+    public :
+        Fixed();
+        Fixed(const int number);
+        Fixed(const float number);
+        ~Fixed();
+        
+        Fixed(const Fixed& aFixed);
+        
+        Fixed& operator=(const Fixed& aFixed);
+        
+        int getRawBits(void)const;
+        void setRawBits(int const raw);
+        float toFloat(void)const;
+        int toInt(void)const;
+
+    private : 
+        int number;
+        static const int bits = 8;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& aFixed);
