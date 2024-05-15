26fa257392259e201545715fe05327ce36e1dc0e
Lucie Gernidos <lgernido@made-f0Cr7s1.clusters.42paris.fr>
2024-05-15 14:42:16 +0200

    fixed problems

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 11:38:44 by lgernido          #+#    #+#             */
-/*   Updated: 2024/05/15 14:00:54 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/05/15 14:41:42 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
     if ((x == ax && y == ay) || (x == bx && y == by) || (x == cx && y == cy))
         return false;
 
-    float areaABP = ((ax - x) * (by - y) - (ay - y) * (bx - x));
-    float areaBCP = ((bx - x) * (cy - y) - (by - y) * (cx - x));
-    float areaCAP = ((cx - x) * (ay - y) - (cy - y) * (ax - x));
+    float triangleABP = ((ax - x) * (by - y) - (ay - y) * (bx - x));
+    float triangleBCP = ((bx - x) * (cy - y) - (by - y) * (cx - x));
+    float triangleCAP = ((cx - x) * (ay - y) - (cy - y) * (ax - x));
 
-    if (areaABP == 0 || areaBCP == 0 || areaCAP == 0)
+    if (triangleABP == 0 || triangleBCP == 0 || triangleCAP == 0)
         return false;
 
-    return (areaABP > 0 && areaBCP > 0 && areaCAP > 0) || (areaABP < 0 && areaBCP < 0 && areaCAP < 0);
+    return (triangleABP > 0 && triangleBCP > 0 && triangleCAP > 0) || (triangleABP < 0 && triangleBCP < 0 && triangleCAP < 0);
 }
